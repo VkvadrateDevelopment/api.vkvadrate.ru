34b63b8f4324f207cef45ea6a3b17cf44c5bcc67
Dulebsky <dulebsky.a@artside.su>
2024-03-06 14:34:10 +0300

    Requests.get bug fixed

--- src/schemas.py
+++ src/schemas.py
@@ -2,15 +2,14 @@ from datetime import datetime
 
 from pydantic import BaseModel
 
-class SOrderUpdate(BaseModel):
-    RequestUrl: str = None
-
 # class SOrderUpdate(BaseModel):
 #     RequestUrl: str = None
-#     ИдентификаторЗаказаVK: str = None
-#     ЗаказКлиента_id: str = None
-#     ДокументОплаты_id: str = None
-#     СуммаОплаты: float = None
+
+class SOrderUpdate(BaseModel):
+    ИдентификаторЗаказаVK: str = None
+    ЗаказКлиента_id: str = None
+    ДокументОплаты_id: str = None
+    СуммаОплаты: float = None
 
 class SOrderResult(BaseModel):
     success: bool
